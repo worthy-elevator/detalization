f0f39a0113e6c6e42b6a80eff15e210e458e03ca
boplkj <boplkj@yandex.ru>
2019-02-21 15:24:04 +0300

    123

--- temp/menu-right.php
+++ temp/menu-right.php
@@ -22,10 +22,21 @@ function menu_right()
 			</div>
 		</div>
 		<div class="menu-right" style="float: right; width:290px">
+			<p class= "servises-name" style = "margin: 10px 0 0 65px; font-size: 25px;"> <a href = "services.php">Услуги</a> </p>
 			<ul style="margin-left:30px; margin-top:20px;">
-				<li style="padding-bottom:5px"><a href="#">художественная ковка</a></li>
-				<li style="padding-bottom:5px"><a href="#">контурная сварка любой сложности</a></li>
-				<li style="padding-bottom:5px"><a href="#">высокоточная фрезеровка</a></li>
+				<li style="padding-bottom:5px"><a href="">Шлифовальная обработка</a></li>
+				<li style="padding-bottom:5px"><a href="#">Токарные работы</a></li>
+				<li style="padding-bottom:5px"><a href="#">Фрезерная обработка</a></li>
+				<li style="padding-bottom:5px"><a href="#">Долбежная обработка</a></li>
+				<li style="padding-bottom:5px"><a href="#">Слесарно-сборочные работы</a></li>
+				<li style="padding-bottom:5px"><a href="#">Сварочные работы</a></li>
+				<li style="padding-bottom:5px"><a href="#">Порошковая покраска</a></li>
+				<li style="padding-bottom:5px"><a href="#">Плазменная резка</a></li>
+				<li style="padding-bottom:5px"><a href="#">Лазерная резка</a></li>
+				<li style="padding-bottom:5px"><a href="#">Гибка и вальцовка</a></li>
+				<li style="padding-bottom:5px"><a href="#">Пусконаладка и ремонт оборудования</a></li>
+				<li style="padding-bottom:5px"><a href="#">Изготовление и заточка промышленных ножей</a></li>
+				<li style="padding-bottom:5px"><a href="#">Изготовление чертежей</a></li>
 			</ul>
 		</div>
 	</div>';
