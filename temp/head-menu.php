06978c76b290eda1f286fdfce57adb957b322010
worthy-elevator <emil_96@inbox.ru>
2019-02-23 00:26:05 +0300

    lolol

--- temp/head-menu.php
+++ temp/head-menu.php
@@ -15,6 +15,19 @@ function menu(){
 <script type="text/javascript" src="./js/script.js"></script>
 </head>
 <body>
+<div id="content" style="text-align:center;position: fixed;right: 40;bottom: 40;">
+    <a href="#" id="callback"><img src="./img/phone.png" style="width: 70;height: 70"></a>
+</div>
+<div id="popup">
+    <form id="contact_form" role="form" method="post" action="./php/order.php">
+        <h3 style = "font-size : 18px; text-align : center;">Укажите свои данные! </h3>
+        <input type="text" name="name"  placeholder="Как к вам обращаться?">
+        <input type="text" name="tel"  class="required" placeholder="Контакный телефон (обязательно)">
+        <input type="text" name="email" placeholder="Email">
+        <textarea name="message" placeholder="Текст сообщения" rows="5"></textarea>
+        <a href="#" class="btn button form_submit">Заказать</a>
+    </form>
+</div>
 	<div class="header">
 		<div class="top-menu">
 			<div class ="buttons" >
