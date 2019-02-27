8e7a8f1b950c0c4fc33d024a73ea73fb6fd6fc24
worthy-elevator <emil_96@inbox.ru>
2019-02-27 15:24:31 +0300

    Update order.php

--- php/order.php
+++ php/order.php
@@ -1,15 +1,7 @@
 <script src='https://www.google.com/recaptcha/api.js'></script>
 <?php
-if(isset($_POST['g-recaptcha-response']) && $_POST['g-recaptcha-response']) 
-{
-	$secret = '6LdtvJMUAAAAAPIwpqx6LC7oAPt9ZRlHDfrExhW_';
-	$ip = $_SERVER['REMOTE_ADDR'];
-	$response = $_POST['g-recaptcha-response'];
-	$rsp = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response&remoteip=$ip");
-	$arr = json_decode($rsp, TRUE);
-	if($arr['success'])
-	{
-		header('Content-type: text/html; charset=utf-8');
+
+
 		error_reporting(0);   
 		if(!empty($_POST['send'])) 
 		{
@@ -33,11 +25,5 @@ if(isset($_POST['g-recaptcha-response']) && $_POST['g-recaptcha-response'])
 		{
 			echo "<h2>Ошибка! Попробуйте еще раз.</h2>";
 		}
-	}
-	else
-	{
-		echo "Вы, кажется робот.";
-	}
-}
 	
 ?>
